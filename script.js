35e5baf789aaca01c8c6e9cf88ff5ac187034078
wanessaideia <148728162+wanessaideia@users.noreply.github.com>
2025-03-31 21:05:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function getResposta() {
+    const resposta = "Você tem 5 cards pendentes no Trello: 'Briefing', 'Design', 'Revisão', 'Aprovação' e 'Entrega'.";
+    document.getElementById('resposta').innerText = resposta;
+}
